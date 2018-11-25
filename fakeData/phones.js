673f07f3c1d49864ad47be16fef66520c675f0cc
AzharAlbakri <albakriazhar.work@gmail.com>
2018-11-25 14:19:56 +0200

    modify database

--- fakeData/phones.js
+++ fakeData/phones.js
@@ -1,167 +1,161 @@
 exports.phones = [
 // Samsung:
   {brand: "Samsung",
-  name: "Galaxy-A6",
-  price: 245.000,
-  img: "img/samsung/Galaxy-A6.png"
+  name: "Galaxy A6",
+  price: 269.99,
+  img: "https://c1.staticflickr.com/5/4861/45991059522_8f35b4582c_o.png"
   },
 
   {brand: "Samsung",
   name: "Galaxy J6 Plus",
   price: 156.994,
-  img: "img/samsung/Galaxy_J6_Plus.jpg"
+  img: "https://c1.staticflickr.com/5/4827/32170068628_f232334ee9_o.png"
   },
 
  {brand: "Samsung",
-  name: "Galaxy-J2",
+  name: "Galaxy J2",
   price: 91.000,
-  img: "img/samsung/Galaxy-J2.jpg"
+  img: "https://c1.staticflickr.com/5/4820/31101968047_bd62911528_o.png"
   },
 
   {brand: "Samsung",
   name: "Galaxy S9",
   price: 839.99,
-  img: "img/samsung/Galaxy_S9.jpeg"
+  img: "https://c1.staticflickr.com/5/4870/31101968197_e1b74a6a26_o.png"
+  },
+////////
+  {brand: "Samsung",
+  name: "Galaxy J5",
+  price: 170.00,
+  img: "https://c1.staticflickr.com/5/4864/31101968327_c70b2cf537_o.png"
   },
 
   {brand: "Samsung",
-  name: "",
-  price: 11,
-  img: "img/samsung/"
+  name: "Galaxy Note 8",
+  price: 750.00,
+  img: "https://c1.staticflickr.com/5/4835/45991059302_1088a59def_o.png"
   },
 
   {brand: "Samsung",
-  name: "",
-  price: 11,
-  img: "img/samsung/"
+  name: "Galaxy J1 Mini Prime",
+  price: 90.00,
+  img: "https://c1.staticflickr.com/5/4866/32170068368_ba40a752e3_o.png"
   },
 
   {brand: "Samsung",
-  name: "",
-  price: 11,
-  img: "img/samsung/"
+  name: "Galaxy A8",
+  price: 800.00,
+  img: "https://c1.staticflickr.com/5/4917/32170068728_b64e1fbefb_o.png"
   },
 
   {brand: "Samsung",
-  name: "",
-  price: 11,
-  img: "img/samsung/"
-  },
-
-  {brand: "Samsung",
-  name: "",
-  price: 11,
-  img: "img/samsung/"
+  name: "Galaxy C7",
+  price: 150.00,
+  img: "https://c1.staticflickr.com/5/4858/31101967937_5964b105b6_o.png"
   },
 
   //apple:
   {brand: "Apple",
   name: "iPhone XS",
-  price: 11,
-  img: "img/apple/Apple-iPhone-XS"
+  price: 999.99,
+  img: "https://c1.staticflickr.com/5/4853/44223321330_874a31e1da_o.png"
   },
 
   {brand: "Apple",
   name: "iPhone 8",
-  price: 11,
-  img: "img/apple/Iphone8_GLD.jpg"
+  price: 599.99,
+  img: "https://c1.staticflickr.com/5/4841/44223321290_329f1d5dda_o.png"
   },
 
   {brand: "Apple",
   name: "iPhone 7",
-  price: 11,
-  img: "img/apple/Apple-iPhone-7.jpg"
+  price: 449.99,
+  img: "https://c1.staticflickr.com/5/4810/44223321460_681da5700d_o.png"
   },
 
   {brand: "Apple",
   name: "iPhone xr",
-  price: 11,
-  img: "img/apple/iphone_xr.jpg"
+  price: 749.99,
+  img: "https://c1.staticflickr.com/5/4896/44223321360_a674b33834_o.png"
   },
 
   {brand: "Apple",
   name: "iPhone se",
-  price: 11,
-  img: "img/apple/iphone_se.jpg"
-  },
-
-  {brand: "Apple",
-  name: "iPhone 10",
-  price: 11,
-  img: "img/apple/iphone10.jpg"
+  price: 344.00,
+  img: "https://c1.staticflickr.com/5/4836/44223321430_58984e66e6_o.png"
   },
 
   {brand: "Apple",
   name: "iPhone 6s plus",
-  price: 11,
-  img: "img/apple/iphone-6s-plus.jpg"
+  price: 399.99,
+  img: "https://c1.staticflickr.com/5/4815/44223321540_6e255d82c3_o.png"
   },
 
   {brand: "Apple",
   name: "iPhone 4S",
-  price: 11,
-  img: "img/apple/iPhone-4S.jpg"
+  price: 250,
+  img: "https://c1.staticflickr.com/5/4828/44223321570_32ffc8f6fb_o.png"
   },
 
   {brand: "Apple",
   name: "iPhone 5S",
-  price: 11,
-  img: "img/apple/iPhone-5S.jpeg"
+  price: 239.00,
+  img: "https://c1.staticflickr.com/5/4835/45127476505_a65885ffec_o.png"
   },
 
   /// huawi
 
   {brand: "Huawei",
   name: "Huawei P10",
-  price: 11,
-  img: "img/huawi/p10-arctic-white.png"
+  price: 220.00,
+  img: "https://c1.staticflickr.com/5/4805/44225202300_6bb5d9dbf8_o.png"
   },
 
   {brand: "Huawei",
   name: "Huawei Mate 20",
-  price: 11,
-  img: "img/huawi/Mate20.png"
+  price: 120.00,
+  img: "https://c1.staticflickr.com/5/4867/45129365395_702517cc59_o.png"
   },
 
   {brand: "Huawei",
   name: "Huawei Nova3",
-  price: 11,
-  img: "img/huawi/nova3.png"
+  price: 450.00,
+  img: "https://c1.staticflickr.com/5/4820/45129365305_9690a592a5_o.png"
   },
 
   {brand: "Huawei",
   name: "Huawei Mate 10",
-  price: 11,
-  img: "img/huawi/mate10.png"
+  price: 200.00,
+  img: "https://c1.staticflickr.com/5/4826/44225202510_7002dafa53_o.png"
   },
 
   {brand: "Huawei",
   name: "Huawei Y9",
-  price: 11,
-  img: "img/huawi/y9.png" //format
+  price: 350.00,
+  img: "https://c1.staticflickr.com/5/4885/44225202140_9e35930499_o.png" //format
   },
 
   {brand: "Huawei",
   name: "Huawei Y6 Prime",
-  price: 11,
-  img: "img/huawi/y6-prime.png"
+  price: 220.00,
+  img: "https://c1.staticflickr.com/5/4809/45129365125_b9ce4a5f21_o.png"
   },
 
   {brand: "Huawei",
   name: "Huawei P10 lite",
-  price: 11,
-  img: "img/huawi/p10-lite.png"
+  price: 170.00,
+  img: "https://c1.staticflickr.com/5/4838/45129365205_2db97911a0_o.png"
   },
 
   {brand: "Huawei",
   name: "Huawei Nova 2",
-  price: 11,
-  img: "img/huawi/nova2.png"
+  price: 140.00,
+  img: "https://c1.staticflickr.com/5/4873/44225202420_e84834dbc7_o.png"
   },
 
   {brand: "Huawei",
   name: "Huawei P smart",
-  price: 11,
-  img: "img/huawi/p-smart.png"
-  },
+  price: 70,
+  img: "https://c1.staticflickr.com/5/4875/46041124321_9e239760a5_o.png"
+  }
 ]
